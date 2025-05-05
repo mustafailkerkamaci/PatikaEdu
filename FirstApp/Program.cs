31b2272ddaa972ee3dd4cdfa394f9f8fb09e7b35
mustafailkerkamaci <mustafailkerkmc@gmail.com>
2025-05-05 17:47:24 +0300

    tanıtıldı

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,7 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Benim adım mustafa !");
+            Console.WriteLine("Mustafa İlke Kamaci");
+            Console.WriteLine("I joined this inspiring and effective bootcamp by Patika to build a strong career in software development and rapidly improve my C# skills!!");
         }
     }
 }
